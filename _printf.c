510dded0b97c9e7f2c356a247bedb8475fb3b542
xhoi700 <10057@holbertonstudents.com>
2024-11-26 13:44:39 +0000

    added new changes

--- _printf.c
+++ _printf.c
@@ -16,7 +16,53 @@ int (*find_handler(char format_char, specifier_t specifiers[]))(va_list)
 }
 int process_format(const char *format, va_list args, specifier_t specifiers[])
 {
-    return (0);
+    unsigned int i = 0;
+    int len = 0;
+    int (*handler)(va_list);
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            if (format[i + 1] == '%')
+            {
+                _putchar('%');
+                len++;
+                i++;
+            }
+            else
+            {
+                handler = find_handler(format[i + 1], specifiers);
+                if (handler)
+                {
+                    len += handler(args);
+                    i++;
+                }
+                else
+                {
+                    _putchar('%');
+                    _putchar(format[i + 1]);
+                    len += 2;
+                    i++;
+                }
+            }
+        }
+        else
+        {
+            _putchar(format[i]);
+            len++;
+        }
+        i++;
+    }
+    return (len);
+
+
+
+
+
+
+
+
+
 }
 int _printf(const char *format, ...)
 {
@@ -27,6 +73,10 @@ int _printf(const char *format, ...)
         {"s", handle_string},
         {NULL, NULL}
     };
+	if (format == NULL || format[0] == '%' && format[1] == '\0')
+	{
+		return (-1);
+	}
     va_start(args, format);
     len = process_format(format, args, specifiers);
     va_end(args);
